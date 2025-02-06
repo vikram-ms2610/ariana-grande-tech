665a396e8186681f071a6beba6269ec0e5a4d8d5
vikramms2610 <vikram2001ms@gmail.com>
2025-02-06 16:58:00 +0530

    navigation changed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route , Navigate} from "react-router-dom";
 import './App.css';
 import Home from './components/Home';
 import Insight from './components/titles/insight'

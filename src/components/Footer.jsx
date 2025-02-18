bd48e632d5d77b04e0e16bcc768b55b26eb6558f
vikram-ms2610 <vikramms2610@gmail.com>
2025-02-18 15:40:55 +0530

    footer changed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,13 +4,9 @@ function Footer() {
   return (
     <footer className="bg-[#212529] text-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
-        {/* Links Section */}
-        <div className="flex flex-col md:flex-row items-center md:justify-between">
-          {/* Content on the left (Empty div to balance layout) */}
-          <div className="hidden md:block"></div>
-
-          {/* Centered Links (Mobile) / Right Aligned (Desktop) */}
-          <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-8 text-center md:text-right md:ml-auto">
+        {/* Links Section (Always Centered) */}
+        <div className="flex justify-center">
+          <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-8 text-center">
             {[
               "Privacy Notice",
               "Cookie Policy",
@@ -27,7 +23,6 @@ function Footer() {
               </a>
             ))}
           </div>
-          <div className="hidden md:block"></div>
         </div>
 
         {/* Line Break */}

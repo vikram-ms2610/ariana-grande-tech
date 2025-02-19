e97d89a2b9fb86d2b262d652386e10ee47550abb
vikram-ms2610 <vikramms2610@gmail.com>
2025-02-19 14:54:26 +0530

    nav gradient color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,21 +31,21 @@ function Navbar() {
                 Insight
               </Link>
             </li> */}
-                         <li>
-               <Link
-                to="/insight"
-               className="p-3 relative group transition-all"
-                     >
-                        Insight
-                    {/* Gradient Text */}
-                   <span className="bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 bg-clip-text text-transparent">
-                         Insight
-                           </span>
+                                 <li>
+                <Link
+                   to="/insight"
+                 className="p-3 relative group transition-all"
+                      >
+                 {/* Gradient Text */}
+                  <span className="bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 bg-clip-text text-transparent">
+                    Insight
+                       </span>
 
-                   {/* Gradient Bottom Border - Initially hidden, expands on hover */}
-                      <span className="absolute bottom-0 left-0 w-0 h-[2px] bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 transition-all group-hover:w-full"></span>
+                  {/* Bottom Border: Initially hidden, will show on hover */}
+                  <span className="absolute bottom-0 left-0 w-0 h-[2px] bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 transition-all group-hover:w-full"></span>
                     </Link>
-                     </li>
+                          </li>
+
 
             <li>
               <Link to="/servicesnav" className="p-3 hover:text-gray-400 transition-all">
